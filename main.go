38365f6e3ab129fdea2101a01e9e608a2d523111
lukechampine <luke.champine@gmail.com>
2020-08-07 21:46:41 -0400

    fix too-aggressive trimming in currencyUnits

--- main.go
+++ main.go
@@ -116,7 +116,8 @@ func plural(n int) string {
 
 func currencyUnits(c types.Currency) string {
 	r := new(big.Rat).SetFrac(c.Big(), types.SiacoinPrecision.Big())
-	return strings.TrimRight(r.FloatString(30), "0.") + "SC"
+	sc := strings.TrimRight(r.FloatString(30), "0")
+	return strings.TrimSuffix(sc, ".") + " SC"
 }
 
 func parseCurrency(s string) types.Currency {
